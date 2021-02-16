a8e1953e55c8f346eb57a7c0b6ef5dedfcf09575
Igor Kuzmenko <igorkuzmenko@Igors-MacBook-Pro.local>
2021-02-16 23:24:50 +0200

    first commit

---
+++ task_one/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace task_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first task");
+
+            string folder_name = "folder";
+
+            for(int i = 0; i <= 10; i++) {
+                DirectoryInfo dirInfo = new DirectoryInfo(folder_name + i.ToString());
+                
+                var x = Directory.CreateDirectory(dirInfo.FullName);
+                
+                Console.WriteLine($"Is directiry exists: {System.IO.Directory.Exists(folder_name + i.ToString())}");
+
+                dirInfo.Delete();
+            }
+        }
+    }
+}
---
+++ task_two/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace task_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task two");
+
+            string fileName = "file1";
+
+            using(System.IO.FileStream fs = System.IO.File.Create(fileName)) {
+                foreach(var b in Encoding.ASCII.GetBytes("some random text")) {
+                    fs.WriteByte(b);
+                }
+                
+            }
+
+            byte[] readBuffer = System.IO.File.ReadAllBytes(fileName);
+            Console.Write(Encoding.ASCII.GetString(readBuffer));
+            Console.WriteLine();
+
+            System.IO.File.Delete(fileName);
+        }
+    }
+}
